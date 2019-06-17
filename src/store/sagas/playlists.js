36b82c3b5fb604363a7d522e8addf16ba1b92673
MarcosMoura <marcos_moura@live.com>
2019-06-17 09:30:35 -0300

    Configurando Ducks e Sagas pra Error

--- src/store/sagas/playlists.js
+++ src/store/sagas/playlists.js
@@ -2,6 +2,7 @@ import { call, put } from 'redux-saga/effects';
 import api from '../../services/api';
 
 import { Creators as PlayListsActions } from '../ducks/playlists';
+import { Creators as ErrorActions } from '../ducks/error';
 
 export function* getplaylists() {
   try {
@@ -9,6 +10,6 @@ export function* getplaylists() {
     const response = yield call(api.get, 'playlists');
     yield put(PlayListsActions.getPlaylistSuccess(response.data));
   } catch (err) {
-    console.log(err);
+    yield put(ErrorActions.setError('Não foi possível obter os detalhes da playlist'));
   }
 }
