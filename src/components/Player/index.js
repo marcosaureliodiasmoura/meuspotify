373ac6d2dffa297f5a58f67dde9c0767040fb8d5
MarcosMoura <marcos_moura@live.com>
2019-06-22 06:19:24 -0300

    Adicionando Next, Prev e Playing (Tempo e Duração, slider)

--- src/components/Player/index.js
+++ src/components/Player/index.js
@@ -20,10 +20,19 @@ import PauseIcon from '../../assets/images/pause.svg';
 import ForwardIcon from '../../assets/images/forward.svg';
 import RepeatIcon from '../../assets/images/repeat.svg';
 
-//vindos do Reducer: player contém os dados do song, Play e Pause são os status do Sound do react-Sound
-const Player = ({ player, play, pause }) => (
+// vindos do Reducer: player contém os dados do song, Play e Pause são os status do Sound do react-Sound
+const Player = ({
+  player, play, pause, next, prev, playing, position, duration,
+}) => (
   <Container>
-    {!!player.currentSong && <Sound url={player.currentSong.file} playStatus={player.status} />}
+    {!!player.currentSong && (
+      <Sound
+        url={player.currentSong.file}
+        playStatus={player.status}
+        onFinishedPlaying={next}
+        onPlaying={playing}
+      />
+    )}
 
     <Current>
       {!!player.currentSong && (
@@ -42,7 +51,7 @@ const Player = ({ player, play, pause }) => (
         <button>
           <img src={ShuffleIcon} alt="Shuffle" />
         </button>
-        <button>
+        <button onClick={prev}>
           <img src={BackwardIcon} alt="Backward" />
         </button>
         {!!player.currentSong && player.status === Sound.status.PLAYING ? (
@@ -54,7 +63,7 @@ const Player = ({ player, play, pause }) => (
             <img src={PlayIcon} alt="play" />
           </button>
         )}
-        <button>
+        <button onClick={next}>
           <img src={ForwardIcon} alt="Foward" />
         </button>
         <button>
@@ -63,7 +72,7 @@ const Player = ({ player, play, pause }) => (
       </Controls>
 
       <Time>
-        <span>1:39</span>
+        <span>{position}</span>
         <ProgressSlider>
           <Slider
             railStyle={{ background: '#404040', borderRadius: 10 }}
@@ -71,7 +80,7 @@ const Player = ({ player, play, pause }) => (
             handleStyle={{ border: 0 }}
           />
         </ProgressSlider>
-        <span>4:24</span>
+        <span>{duration}</span>
       </Time>
     </Progress>
 
@@ -96,10 +105,26 @@ Player.propTypes = {
   }).isRequired,
   play: PropTypes.func.isRequired,
   pause: PropTypes.func.isRequired,
+  next: PropTypes.func.isRequired,
+  prev: PropTypes.func.isRequired,
+  playing: PropTypes.func.isRequired,
+  position: PropTypes.string.isRequired,
+  duration: PropTypes.string.isRequired,
 };
+
+function msToTime(duration) {
+  let seconds = parseInt((duration / 1000) % 60, 10);
+  const minutes = parseInt((duration / (1000 * 60)) % 60, 10);
+
+  seconds = seconds < 10 ? `0${seconds}` : seconds;
+
+  return `${minutes}:${seconds}`;
+}
 
 const mapStateToProps = state => ({
   player: state.player,
+  position: msToTime(state.player.position),
+  duration: msToTime(state.player.duration),
 });
 
 const mapDispatchToProps = dispatch => bindActionCreators(PlayerActions, dispatch);
